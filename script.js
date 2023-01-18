5a7cdc3462bc0a9905a5ea943490408333f6db62
EugeneTsalko <tefworkmail@gmail.com>
2023-01-18 22:46:16 +0300

    feat: add some functional

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log('Hello World!');
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.nav-wrapper');
+const overlay = document.querySelector('.overlay');
+
+const toggleBurger = () => {
+    burger.classList.toggle('active');
+    nav.classList.toggle('active');
+    overlay.classList.toggle('active');
+}
+
+const closeBurger = (event) => {
+    if (event.target.classList.contains('nav-item') || (event.target.classList.contains('overlay'))) {
+        burger.classList.remove('active');
+        nav.classList.remove('active');
+        overlay.classList.remove('active');
+    }
+}
+
+burger.addEventListener('click', toggleBurger);
+nav.addEventListener('click', closeBurger);
+overlay.addEventListener('click', closeBurger);
